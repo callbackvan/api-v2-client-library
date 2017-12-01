75e4e5a5ecbd1859996337fca5c9c6f2172c9864
aek <aek@callbackhunter.com>
2017-12-01 15:11:32 +0300

    изображения

---
+++ src/Entity/Widget/Settings/Images/Images.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Entity\Widget\Settings\Images;
+
+class Images
+{
+    /** @var string */
+    private $buttonLogo = '';
+
+    /** @var string */
+    private $iconLogoSlider = '';
+
+    /** @var string */
+    private $backgroundSlider = '';
+
+    /**
+     * @return string
+     */
+    public function getButtonLogo()
+    {
+        return $this->buttonLogo;
+    }
+
+    /**
+     * @param string $buttonLogo
+     *
+     * @return $this
+     */
+    public function setButtonLogo($buttonLogo)
+    {
+        $this->buttonLogo = $buttonLogo;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIconLogoSlider()
+    {
+        return $this->iconLogoSlider;
+    }
+
+    /**
+     * @param string $iconLogoSlider
+     *
+     * @return $this
+     */
+    public function setIconLogoSlider($iconLogoSlider)
+    {
+        $this->iconLogoSlider = $iconLogoSlider;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBackgroundSlider()
+    {
+        return $this->backgroundSlider;
+    }
+
+    /**
+     * @param string $backgroundSlider
+     *
+     * @return $this
+     */
+    public function setBackgroundSlider($backgroundSlider)
+    {
+        $this->backgroundSlider = $backgroundSlider;
+
+        return $this;
+    }
+}
