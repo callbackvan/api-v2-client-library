2e48d20aa51fc0490e071feda4430c607b07ffe1
artur <artur@callbackmail.com>
2018-02-14 18:42:55 +0300

    Добавлено channelId для vk

--- src/Entity/Widget/Settings/Channels/Channels.php
+++ src/Entity/Widget/Settings/Channels/Channels.php
@@ -19,6 +19,12 @@ class Channels implements BaseEntityInterface
             'skype',
         ];
 
+    const AMO_CHANNELS_LIST
+        = [
+            'vk',
+            'facebook',
+        ];
+
     /** @var Channel */
     private $callback;
     /** @var Channel */
@@ -208,7 +214,7 @@ class Channels implements BaseEntityInterface
             try {
                 $toApi = $this->get($cName)->toAPI();
 
-                if ($cName !== 'facebook') {
+                if (!in_array($cName, self::AMO_CHANNELS_LIST, true)) {
                     unset($toApi['channelId']);
                 }
 
